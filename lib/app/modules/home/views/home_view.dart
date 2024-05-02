e3be172d12bb2ef670bc1ea99ae056779ebe6bd7
Rudy Nurafif <rudynurafif@gmail.com>
2024-05-02 19:11:28 +0700

    Media Query

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,17 +8,17 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    double widthDevice = MediaQuery.of(context).size.width;
+    double heightDevice = MediaQuery.of(context).size.height;
+    double paddingTop = MediaQuery.of(context).padding.top;
+    double paddingBottom = MediaQuery.of(context).padding.bottom;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        body: Container(
+      margin: EdgeInsets.only(top: paddingTop, bottom: paddingBottom),
+      width: widthDevice * 0.5,
+      height: heightDevice - paddingTop - paddingBottom,
+      color: Colors.red,
+    ));
   }
 }
