aff802ad3a17fcd68a977a7d87ef3a7af9617e3b
Rudy Nurafif <rudynurafif@gmail.com>
2024-05-06 14:56:05 +0700

    Hero widget

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,21 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../../routes/app_pages.dart';
-import '../controllers/home_controller.dart';
+import '../controllers/profile_controller.dart';
 
-class HomeView extends GetView<HomeController> {
-  const HomeView({super.key});
-
+class ProfileView extends GetView<ProfileController> {
+  const ProfileView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hero"),
+        title: const Text('ProfileView'),
         centerTitle: true,
       ),
       body: Center(
@@ -25,8 +22,8 @@ class HomeView extends GetView<HomeController> {
             tag: "tag1",
             child: ClipOval(
               child: Container(
-                width: 50,
-                height: 50,
+                width: 300,
+                height: 300,
                 child: Image.network(
                   "https://picsum.photos/id/237/200/300",
                   fit: BoxFit.cover,
