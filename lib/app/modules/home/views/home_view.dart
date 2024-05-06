47b50c1776184e2826f9e020d157d1f5b3921867
Rudy Nurafif <rudynurafif@gmail.com>
2024-05-06 10:32:09 +0700

    Get X feature

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,13 +7,19 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    double widthDevice = MediaQuery.of(context).size.width;
-    double heightDevice = MediaQuery.of(context).size.height;
-    double paddingTop = MediaQuery.of(context).padding.top;
-    double paddingBottom = MediaQuery.of(context).padding.bottom;
+    double widthDevice = Get.width;
+    double heightDevice = Get.height;
+    double paddingTop = context.mediaQueryPadding.top;
+    double paddingBottom = context.mediaQueryPadding.bottom;
+
+    // double widthDevice = MediaQuery.of(context).size.width;
+    // double heightDevice = MediaQuery.of(context).size.height;
+    // double paddingTop = MediaQuery.of(context).padding.top;
+    // double paddingBottom = MediaQuery.of(context).padding.bottom;
 
     AppBar myAppBar() {
       return AppBar(
@@ -25,102 +31,9 @@ class HomeView extends GetView<HomeController> {
     double heightBody = heightDevice - myAppBar().preferredSize.height - paddingTop;
 
     return Scaffold(
-      appBar: myAppBar(),
-      body: Wrap(
-        direction: Axis.vertical,
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            width: widthDevice * 0.2,
-            height: 35,
-            color: Colors.amber,
-          ),
-        ],
-      ),
-    );
+        appBar: myAppBar(),
+        body: Container(
+          width: Get.width,
+        ));
   }
 }
